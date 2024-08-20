2a165947fa93e5dab2b78767d3eba78d46703397
Mark-crypto <Mark.Onyango@strathmore.edu>
2024-08-20 10:51:47 +0300

    adding login

---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,36 @@
+export const Login = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form submitted");
+  };
+  const handleInput = (e) => {
+    console.log(e.target.value);
+  };
+  return (
+    <>
+      <form onSubmit={handleSubmit} className="login-form">
+        <h4>GYM BROS</h4>
+        <label htmlFor="email">Email Address</label>
+        <br />
+        <input type="text" name="email" id="email" onChange={handleInput} />
+        <br />
+        <label htmlFor="password">Password</label>
+        <br />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          onChange={handleInput}
+        />{" "}
+        <br />
+        <button type="submit" className="login-btn">
+          Login
+        </button>{" "}
+        <br />
+        <p className="login-text">
+          Don't have an account? <a href="">Sign up</a>
+        </p>
+      </form>
+    </>
+  );
+};
